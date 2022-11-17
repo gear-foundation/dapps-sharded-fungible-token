babe436f9ec1390c169970d753efb9c2529bf718
Mikita <strizhak.ni@phystech.edu>
2022-11-18 02:57:46 +0300

    - Implement Permit for token - Checking signature to protect permit - Implement permit_id for better safety - Cover token with functionality ad safety test for permit
    Signed-off-by: MikitaS <mikita.stryzhak@gmail.com>

--- ft-main/tests/utils.rs
+++ ft-main/tests/utils.rs
@@ -1,11 +1,15 @@
 use ft_logic_io::{Action, FTLogicEvent};
 use ft_main_io::*;
-use gstd::prelude::*;
+use gstd::{prelude::*, ActorId};
 use gtest::{Program, System};
+use sp_core::sr25519::Signature;
+
 pub trait FToken {
     fn ftoken(system: &System) -> Program;
     fn mint(&self, transaction_id: u64, from: u64, account: u64, amount: u128, error: bool);
     fn check_balance(&self, account: u64, expected_amount: u128);
+    fn check_balance_actor_id(&self, account: ActorId, expected_amount: u128);
+    fn check_permit_id(&self, account: ActorId, expected_permit_id: u128);
     fn burn(&self, transaction_id: u64, from: u64, account: u64, amount: u128, error: bool);
     fn transfer(
         &self,
@@ -13,6 +17,15 @@ pub trait FToken {
         from: u64,
         sender: u64,
         recipient: u64,
+        amount: u128,
+        error: bool,
+    );
+    fn transfer_actor_id(
+        &self,
+        transaction_id: u64,
+        from: u64,
+        sender: ActorId,
+        recipient: ActorId,
         amount: u128,
         error: bool,
     );
@@ -24,8 +37,23 @@ pub trait FToken {
         amount: u128,
         error: bool,
     );
+
+    #[allow(clippy::too_many_arguments)]
+    fn permit(
+        &self,
+        transaction_id: u64,
+        from: u64,
+        owner: ActorId,
+        approved_account: ActorId,
+        amount: u128,
+        permit_id: u128,
+        sign: Signature,
+        error: bool,
+    );
     fn send_message_and_check_res(&self, from: u64, payload: FTokenAction, error: bool);
 }
+
+const HARDCODED_ACCOUNT: u64 = 100;
 
 impl FToken for Program<'_> {
     fn ftoken(system: &System) -> Program {
@@ -39,7 +67,7 @@ impl FToken for Program<'_> {
             .into();
 
         let res = ftoken.send(
-            100,
+            HARDCODED_ACCOUNT,
             InitFToken {
                 storage_code_hash: storage_code_hash.into(),
                 ft_logic_code_hash: ft_logic_code_hash.into(),
@@ -106,6 +134,31 @@ impl FToken for Program<'_> {
         );
     }
 
+    fn transfer_actor_id(
+        &self,
+        transaction_id: u64,
+        from: u64,
+        sender: ActorId,
+        recipient: ActorId,
+        amount: u128,
+        error: bool,
+    ) {
+        let payload = Action::Transfer {
+            sender,
+            recipient,
+            amount,
+        }
+        .encode();
+        self.send_message_and_check_res(
+            from,
+            FTokenAction::Message {
+                transaction_id,
+                payload,
+            },
+            error,
+        );
+    }
+
     fn approve(
         &self,
         transaction_id: u64,
@@ -129,10 +182,51 @@ impl FToken for Program<'_> {
         );
     }
 
+    fn permit(
+        &self,
+        transaction_id: u64,
+        from: u64,
+        owner: ActorId,
+        approved_account: ActorId,
+        amount: u128,
+        permit_id: u128,
+        sign: Signature,
+        error: bool,
+    ) {
+        let payload = Action::Permit {
+            owner_account: owner,
+            approved_account,
+            amount,
+            permit_id,
+            sign: sign.into(),
+        }
+        .encode();
+        self.send_message_and_check_res(
+            from,
+            FTokenAction::Message {
+                transaction_id,
+                payload,
+            },
+            error,
+        );
+    }
+
     fn check_balance(&self, account: u64, expected_amount: u128) {
-        let res = self.send(100, FTokenAction::GetBalance(account.into()));
+        let res = self.send(HARDCODED_ACCOUNT, FTokenAction::GetBalance(account.into()));
         let reply = FTLogicEvent::Balance(expected_amount).encode();
-        assert!(res.contains(&(100, reply)));
+        assert!(res.contains(&(HARDCODED_ACCOUNT, reply)));
+    }
+
+    fn check_balance_actor_id(&self, account: ActorId, expected_amount: u128) {
+        let res = self.send(HARDCODED_ACCOUNT, FTokenAction::GetBalance(account));
+        let reply = FTLogicEvent::Balance(expected_amount).encode();
+        assert!(res.contains(&(HARDCODED_ACCOUNT, reply)));
+    }
+
+    fn check_permit_id(&self, account: ActorId, expected_permit_id: u128) {
+        let res = self.send(HARDCODED_ACCOUNT, FTokenAction::GetPermitId(account));
+        let reply = FTLogicEvent::PermitId(expected_permit_id).encode();
+        assert!(res.contains(&(HARDCODED_ACCOUNT, reply)));
     }
 
     fn send_message_and_check_res(&self, from: u64, payload: FTokenAction, error: bool) {
