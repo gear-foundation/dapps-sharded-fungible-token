bf7b5258754822da03d823d00128e09064412e83
LouiseMedova <luisa.medova@gmail.com>
2022-11-30 14:05:25 +0300

    batch mint

--- ft-main/tests/node-test.rs
+++ ft-main/tests/node-test.rs
@@ -18,14 +18,15 @@
 
 //! Test for harmful demos, checking their init can't brake the chain.
 
+use blake2_rfc::blake2b;
 use ft_logic_io::Action;
 use ft_main_io::*;
 use gclient::{EventListener, EventProcessor, GearApi, Result};
+use gear_core::ids::ProgramId;
 use gstd::CodeId;
 use gstd::{prelude::*, ActorId};
 use primitive_types::H256;
 use std::time::Duration;
-use blake2_rfc::blake2b;
 
 const HASH_LENGTH: usize = 32;
 type Hash = [u8; HASH_LENGTH];
@@ -37,18 +38,21 @@ const PATHS: [&str; 3] = [
 ];
 
 static mut TOKEN_ID: [u8; 32] = [0; 32];
+const FTOKEN: [u8; 32] =
+    hex_literal::hex!("ab240e729b911b9e16b72dfd4e65d22979678aab9049082de9ad030c81fd31f7");
+// 0x4c6eedf8f825338c8ab309729c53dfa7de7f639181111cc850dd06c3496de976
 async fn upload_programs_and_check(api: &GearApi, listener: &mut EventListener) -> Result<()> {
     // upload codes for main fungible token contract
     let mut storage_code_id: Hash = Default::default();
     let storage_code = gclient::code_from_os(PATHS[2])?;
-    storage_code_id[..].copy_from_slice(blake2b::blake2b(HASH_LENGTH, &[], &storage_code).as_bytes());
+    storage_code_id[..]
+        .copy_from_slice(blake2b::blake2b(HASH_LENGTH, &[], &storage_code).as_bytes());
     api.upload_code(storage_code).await;
 
     let mut logic_code_id: Hash = Default::default();
     let logic_code = gclient::code_from_os(PATHS[1])?;
     logic_code_id[..].copy_from_slice(blake2b::blake2b(HASH_LENGTH, &[], &logic_code).as_bytes());
     api.upload_code(logic_code).await;
-   
 
     let init_ft_payload = unsafe {
         InitFToken {
@@ -70,7 +74,7 @@ async fn upload_programs_and_check(api: &GearApi, listener: &mut EventListener) 
         )
         .await?;
 
-        // Program initialization.
+    // Program initialization.
     let (mid, _pid, _) = api
         .upload_program_bytes_by_path(
             PATHS[0],
@@ -103,11 +107,11 @@ async fn mint_message() -> Result<()> {
 
     upload_programs_and_check(&api, &mut listener).await?;
 
-    let account: u64 = 1;
-    let transaction_id: u64 = 0;
+    let mut account: u64 = 1;
+    let mut transaction_id: u64 = 0;
     let amount: u128 = 10_000;
 
-    let mint_payload = FTokenAction::Message {
+    let mut mint_payload = FTokenAction::Message {
         transaction_id,
         payload: Action::Mint {
             recipient: account.into(),
@@ -116,18 +120,55 @@ async fn mint_message() -> Result<()> {
         .encode(),
     };
 
-    let gas_info = unsafe {
-        api.calculate_handle_gas(None, TOKEN_ID.into(), mint_payload.encode(), 0, true, None)
-            .await?
-    };
+    // let gas_info = unsafe {
+    //     api.calculate_handle_gas(None, TOKEN_ID.into(), mint_payload.encode(), 0, true, None)
+    //         .await?
+    // };
 
-    let (mid, _) = unsafe {
-        api.send_message(TOKEN_ID.into(), mint_payload, gas_info.min_limit, 0)
-            .await?
-    };
+    // println!("GAS {:?}", gas_info.min_limit);
+    // println!("GAS {:?}", gas_info.min_limit * 10 / 11);
+    // let (mid, _) = unsafe {
+    //     api.send_message(
+    //         TOKEN_ID.into(),
+    //         mint_payload,
+    //         gas_info.min_limit * 10 / 11,
+    //         0,
+    //     )
+    //     .await?
+    // };
 
-    // Asserting successful initialization.
-    assert!(listener.message_processed(mid).await?.succeed());
+    for i in 0..10000 {
+        let gas_info = unsafe {
+            api.calculate_handle_gas(None, TOKEN_ID.into(), mint_payload.encode(), 0, true, None)
+                .await?
+        };
+
+        println!("GAS {:?}", gas_info.min_limit);
+        println!("iteration {:?}", i);
+        let (mid, _) = unsafe {
+            api.send_message(
+                TOKEN_ID.into(),
+                mint_payload.clone(),
+                250000000000,
+                0,
+            )
+            .await?
+        };
+        account = account + 1;
+        transaction_id = transaction_id + 1;
+
+        mint_payload = FTokenAction::Message {
+            transaction_id,
+            payload: Action::Mint {
+                recipient: account.into(),
+                amount,
+            }
+            .encode(),
+        };
+
+        // Asserting successful initialization.
+        assert!(listener.message_processed(mid).await?.succeed());
+    }
 
     // Checking that blocks still running.
     assert!(listener.blocks_running().await?);
@@ -135,3 +176,123 @@ async fn mint_message() -> Result<()> {
     Ok(())
 }
 
+#[tokio::test]
+async fn continue_mint() -> Result<()> {
+    // Creating gear api.
+    //
+    // By default, login as Alice, than re-login as Bob.
+    let api = GearApi::dev().await?.with("//Bob")?;
+    let mut listener = api.subscribe().await?;
+
+    let mut account: u64 = 1000;
+    let mut transaction_id: u64 = 1000;
+    let amount: u128 = 10_000;
+
+    let mut mint_payload = FTokenAction::Message {
+        transaction_id,
+        payload: Action::Mint {
+            recipient: account.into(),
+            amount,
+        }
+        .encode(),
+    };
+
+    for i in 1000..10000 {
+        let gas_info = unsafe {
+            api.calculate_handle_gas(None, FTOKEN.into(), mint_payload.encode(), 0, true, None)
+                .await?
+        };
+
+        println!("GAS {:?}", gas_info.min_limit);
+        println!("GAS {:?}", gas_info.min_limit + gas_info.min_limit / 5);
+        println!("iteration {:?}", i);
+        let (mid, _) = unsafe {
+            api.send_message(
+                FTOKEN.into(),
+                mint_payload.clone(),
+                gas_info.min_limit + gas_info.min_limit / 5,
+                0,
+            )
+            .await?
+        };
+        account = account + 1;
+        transaction_id = transaction_id + 1;
+
+        mint_payload = FTokenAction::Message {
+            transaction_id,
+            payload: Action::Mint {
+                recipient: account.into(),
+                amount,
+            }
+            .encode(),
+        };
+
+        // Asserting successful initialization.
+        assert!(listener.message_processed(mid).await?.succeed());
+    }
+
+    // Checking that blocks still running.
+    assert!(listener.blocks_running().await?);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn batch_mint() -> Result<()> {
+    // Creating gear api.
+    //
+    // By default, login as Alice, than re-login as Bob.
+    let api = GearApi::dev().await?.with("//Bob")?;
+    let mut listener = api.subscribe().await?;
+
+    let mut account: u64 = 1004;
+    let mut transaction_id: u64 = 1004;
+    let amount: u128 = 10_000;
+
+    let mut payloads: Vec<Vec<u8>> = Vec::new();
+
+    for i in 1..40 {
+        let mint_payload = FTokenAction::Message {
+            transaction_id,
+            payload: Action::Mint {
+                recipient: account.into(),
+                amount,
+            }
+            .encode(),
+        }.encode();
+        account = account + 1;
+        transaction_id = transaction_id + 1;
+        payloads.push(mint_payload);
+    }
+
+    let payloads_len = payloads.len();
+
+    // Sending batch.
+    let args: Vec<_> = payloads
+        .into_iter()
+        .map(|payload| ( FTOKEN.into(), payload, 250000000000, 0))
+        .collect();
+
+    let (ex_res, _)  =
+        api.send_message_bytes_batch(args).await?;
+
+    
+    // Ids of initial messages.
+    let mids: Vec<_> = ex_res
+    .into_iter()
+    .filter_map(|v| v.ok().map(|(mid, _pid)| mid))
+    .collect();
+
+    // Checking that all upload program calls succeed in batch.
+    assert_eq!(payloads_len, mids.len());
+
+    // Checking that all batch got processed.
+    assert_eq!(
+    payloads_len,
+    listener.message_processed_batch(mids).await?.len(),
+    );
+    // Checking that blocks still running.
+    assert!(listener.blocks_running().await?);
+
+    Ok(())
+}
